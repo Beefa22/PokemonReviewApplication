7b30c4440a509ab25e100e806cc2db283d489fee
YoussefMaklad <118008796+Beefa22@users.noreply.github.com>
2023-10-14 20:36:09 +0300

    Add project files.

---
+++ PokemonReviewApplication/Models/Pokemon.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace PokemonReviewApplication.Models
+{
+	public class Pokemon
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public DateTime DateOfBirth { get; set; }
+
+	}
+}
---
+++ PokemonReviewApplication/Models/Reviewer.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApplication.Models
+{
+	public class Reviewer
+	{
+		public int Id { get; set; }
+		public string FirtName { get; set; }
+		public string LastName { get; set; }
+		public string Test { get; set; }
+	}
+}
